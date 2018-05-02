6240d033a9e778c34560e496641d58bd9e89c712
redpopcornv <redpopcornv@naver.com>
2018-05-02 15:54:01 +0900

    led 쉬프터 추가

--- exam/main.cpp
+++ exam/main.cpp
@@ -5,11 +5,18 @@
 int main(void)
 {
 	DDRD = 0xff;
+	PORTD = 0x00;
 	while (1)
 	{
-		PORTD=0xff;
-		_delay_ms(300);
-		PORTD=0x00;
-		_delay_ms(300);
+		for(int i=0;i<7;i++)
+		{
+			PORTD= ~(1<<i);
+			_delay_ms(300);
+		}
+		for(int i=7;i>0;i--)
+		{
+			PORTD = ~(1<<i);
+			_delay_ms(300);
+		}
 	}
 }
