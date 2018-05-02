7dd6c0448a851a44b0f1153d39f0c0daf07aabc2
redpopcornv <redpopcornv@naver.com>
2018-05-02 12:15:45 +0900

    shift기능으로 변경

--- LED_ONOFF/LED_ONOFF/main.cpp
+++ LED_ONOFF/LED_ONOFF/main.cpp
@@ -15,6 +15,16 @@ int main(void)
 		_delay_ms(500);
 		PORTD=0xff;
 		_delay_ms(500);
+		for(int i=0;i<7;i++)
+		{
+			PORTD= ~(1<<i);
+			_delay_ms(500);
+		}
+		for(int i=7;i>=0;i--)
+		{
+			PORTD = ~(1<<i);
+			_delay_ms(500);
+		}
     }
 }
 
