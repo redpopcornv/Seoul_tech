74240a179108939e4e394e3bfb458b31f88b90e6
redpopcornv <redpopcornv@naver.com>
2018-05-02 11:33:30 +0900

    LED 등록

---
+++ LED_ONOFF/LED_ONOFF/main.cpp
@@ -0,0 +1,20 @@
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	
+	DDRD=0xff;
+	PORTD=0xff;
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTD=0x00;
+		_delay_ms(500);
+		PORTD=0xff;
+		_delay_ms(500);
+    }
+}
+
